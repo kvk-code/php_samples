6d47f73163ee6fb0b9f7bc4a2d08ad7ae1b43e5a
KIRAN V K <75604208+kvk-code@users.noreply.github.com>
2023-12-04 08:32:51 +0530

    alter and create

---
+++ php_mysql/create_table.php
@@ -0,0 +1,37 @@
+<?php
+// Database connection parameters
+$host = 'localhost';    // Replace with your host name
+$db = 'college';        // Replace with your database name
+$user = 'root';         // Replace with your database username
+$pass = '';             // Replace with your database password
+$charset = 'utf8mb4';
+
+// Data Source Name
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    // Creating PDO instance
+    $pdo = new PDO($dsn, $user, $pass);
+    // Setting error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // SQL to create 'students' table
+    $sql = "
+    CREATE TABLE IF NOT EXISTS students (
+        name VARCHAR(100),
+        rollno INT,
+        department VARCHAR(20),
+        university_regno VARCHAR(50) PRIMARY KEY,
+        FOREIGN KEY (department) REFERENCES department(shortform)
+    )";
+
+    // Executing the SQL query
+    $pdo->exec($sql);
+
+    // Success message
+    echo "Table 'students' created successfully";
+} catch (PDOException $e) {
+    // Error handling
+    echo "Error creating table: " . $e->getMessage();
+}
+?>
