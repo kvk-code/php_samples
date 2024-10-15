631810954fde9dc35ac8d40092a10cc40f8f4873
KIRAN V K <75604208+kvk-code@users.noreply.github.com>
2024-10-15 13:26:43 +0530

    Add files via upload
    with session

---
+++ php_form_samples/student_session/stud_add_sess.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorial Calculator</title>
+</head>
+<body>
+    <?php
+    session_start();
+    if (!isset($_SESSION['students'])) {
+        $_SESSION['students'] = [];
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['name'];
+        $score = $_POST['score'];
+        $_SESSION['students'][$name] = $score;
+    }
+
+    $students = $_SESSION['students'];
+    
+    echo "<table border='1'>";
+    echo "<tr><th>Student Name</th><th>Score</th></tr>";
+    foreach ($students as $studentName => $studentScore) {
+        echo "<tr><td>$studentName</td><td>$studentScore</td></tr>";
+    }
+    echo "</table>";
+    ?>
+
+    
+</body>
+</html>
